ac75813d5eba6d072de5bd698bc7bdb121b00f95
Wesley-devr <wesleyalexenv@gmail.com>
2021-04-30 18:25:50 -0300

    adicionando pasta

---
+++ humanae/humanae/src/test/java/com/humanae/humanae/HumanaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanae.humanae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
