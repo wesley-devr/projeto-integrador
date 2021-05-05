e6440033b25aca0b26d473401cc05e60e9b49f58
Wesley-devr <wesleyalexenv@gmail.com>
2021-05-05 16:04:54 -0300

    Atualizando

--- humanae/humanae/src/main/java/com/humanae/humanae/controllers/ParceirosController.java
+++ humanae/humanae/src/main/java/com/humanae/humanae/controllers/ParceirosController.java
@@ -4,6 +4,7 @@ package com.humanae.humanae.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ import com.humanae.humanae.repositories.ParceirosRepository;
 @RequestMapping("/parceiros")
 public class ParceirosController {
 
+	@Autowired
 	private ParceirosRepository repository;
 	
 	@GetMapping
