f35548c2e8b8f1676d7dfbdc9f7ad6c8d3a06ba1
jemouraa <jecruz12@gmail.com>
2021-05-06 15:19:26 -0300

    Task 6

--- humanae/humanae/src/main/java/com/humanae/humanae/models/Parceiros.java
+++ humanae/humanae/src/main/java/com/humanae/humanae/models/Parceiros.java
@@ -1,11 +1,18 @@
 package com.humanae.humanae.models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class Parceiros {
@@ -45,6 +52,10 @@ public class Parceiros {
 	@NotNull
 	@Size(min = 5, max = 45)
 	private String conta;
+	
+	@OneToMany(mappedBy = "parceiros" )
+	@JsonIgnoreProperties
+	private List<Produtos> produto;
 
 	public long getId() {
 		return id;
--- humanae/humanae/src/main/java/com/humanae/humanae/models/Produtos.java
+++ humanae/humanae/src/main/java/com/humanae/humanae/models/Produtos.java
@@ -1,11 +1,16 @@
 package com.humanae.humanae.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class Produtos {
@@ -28,6 +33,10 @@ public class Produtos {
 	@Size(min = 5, max = 255)
 	private String descricao;
 
+	@ManyToOne
+	@JoinColumn(name="id_parceiros")
+	private Parceiros parceiros;
+	
 	public long getId() {
 		return id;
 	}
