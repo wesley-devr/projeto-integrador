7bc3e377c3b421f29a9226a0f39fadd4750fe168
jemouraa <jecruz12@gmail.com>
2021-06-05 23:12:29 -0300

    implementando o componente parceiros

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 import { MenuComponent } from './menu/menu.component';
+import { ParceirosComponent } from './parceiros/parceiros.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    ParceirosComponent
   ],
   imports: [
     BrowserModule,
